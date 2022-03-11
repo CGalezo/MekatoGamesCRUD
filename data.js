285cb853d43cb329133dbb0789e7a272b488f2a5
cgalezo <cgalezo@uninorte.edu.co>
2022-03-10 19:29:00 -0500

    Add database

---
+++ data.js
@@ -0,0 +1,14 @@
+const data = {
+    message: "THE data",
+    characters: [],
+    missions: [],
+    models_3d: [],
+    players: [],
+    missions_objectives: [],
+    images_2d: [],
+    player_characters: [],
+    character_stats: [],
+    items: []
+}
+
+export default data
