14572eb7e0412d1bebcce294f77cfeca0e79f3a3
jgmaza <jgmaza@uninorte.edu.co>
2022-03-10 20:18:43 -0500

    corregido ahora si

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const { images_2d } = require('./data');
 const characters = require('./models/characters')
 const characterStats = require('./models/characterStats')
 const images2d = require('./models/images2d')
-const item = require('./models/item')
+const items = require('./models/items')
 const missionObjectives = require('./models/missionObjectives')
 const missions = require('./models/missions')
 const models3d = require('./models/models3d')
 const playerCharacter = require('./models/playerCharacter')
-const player = require('./models/player')
+const players = require('./models/players')
 
 
 const server = new Server();
@@ -17,12 +17,12 @@ const server = new Server();
 server.app.use('/characters', characters);
 server.app.use('/characterStats', characterStats);
 server.app.use('/images2d', images_2d);
-server.app.use('/item', item);
+server.app.use('/items', items);
 server.app.use('/missionObjectives', missionObjectives);
 server.app.use('/missions', missions);
 server.app.use('/models3d', models3d);
 server.app.use('/playerCharacter', playerCharacter);
-server.app.use('/player', player);
+server.app.use('/players', players);
 
 
 //Server Start
